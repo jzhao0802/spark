ab1ad6606321a366498a0a091dc847763b136665
Lichao Wang (Dell laptop) <lichao.wang@uk.imshealth.com>
2016-10-05 12:03:34 +0100

    implementation just finished for using the built-in CrossValidator for elastic-net linear regression.

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -1,21 +1,27 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, DoubleType, IntegerType, StringType
 from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 from pyspark.ml.regression import LinearRegression
 from pyspark.ml.evaluation import RegressionEvaluator
 
 
 
 def main():
+    nEvalFolds = 5
+    nValiFolds = 5
+    seed = 1.0
+    # hyper-params
+    logLambdas = list(range(-2,3,1))
+    lambdas = exp(logLambdas)
+    alphas = list(range(0,1.01,0.1))
+
     dataFileName = "s3://emr-rwes-pa-spark-dev-datastore/lichao.test/data/toy_data/data_linear_regression.csv"
     spark = SparkSession\
            .builder\
            .appName("Linear Regression")\
            .getOrCreate()
     
-    # schema = StructType([StructField("y", DoubleType()),
-                         # StructField("V1", DoubleType()),
-                         # StructField("C", StringType())])
     data = spark\
           .read\
           .option("header", "true")\
@@ -23,20 +29,39 @@ def main():
           .csv(dataFileName)
           
     assembler = VectorAssembler(inputCols=data.columns[1:], outputCol="features")
-    featureAssembledData = assembler.transform(data).select("y", "features")
+    featureAssembledData = assembler.transform(data).select("y", "features")    
+    evalRandColAppendedData = featureAssembledData.select("*", rand(seed).alias(randCol))
+    
     lr = LinearRegression(maxIter=1e5, regParam=0.01, elasticNetParam=0.9, standardization=True, 
                           featuresCol="features", labelCol="y")
-    
-    model = lr.fit(featureAssembledData)
-
-    # # Print the coefficients and intercept for linear regression
-    # print("Coefficients: " + str(model.coefficients))
-    # print("Intercept: " + str(model.intercept))
-    
-    prediction = model.transform(featureAssembledData)
-    
     evaluator = RegressionEvaluator(predictionCol="prediction", labelCol="y")
-    metric = evaluator.evaluate(prediction)
+    paramGrid = ParamGridBuilder()\
+               .addGrid(lr.regParam, lambdas)\
+               .addGrid(lr.elasticNetParam, alphas)\
+               .build()    
+    
+    h = 1.0 / nEvalFolds
+    randCol = "_rand"
+    
+    predictionsAllData = None
+    for iFold in range(nEvalFolds):
+        lb = iFold * h
+        ub = (iFold + 1) * h
+        condition = (evalRandColAppendedData[randCol] >= lb) & (evalRandColAppendedData[randCol] < ub)
+        testData = evalRandColAppendedData.filter(condition)
+        trainData = evalRandColAppendedData.filter(~condition)
+        
+        validator = CrossValidator(estimator=lr, estimatorParamMaps=paramGrid, evaluator=evaluator)
+        cvModel = cv.fit(trainData)
+        predictions = cvModel.transform(testData)
+        
+        if predictionsAllData is not None:
+            predictionsAllData = predictionsAllData.unionAll(predictions)
+        else:
+            predictionsAllData = predictions
+    
+    rmse = evaluator.evaluate(predictionsAllData)
+    print("Final rmse: {}".format(rmse))
     
     spark.stop()
 
