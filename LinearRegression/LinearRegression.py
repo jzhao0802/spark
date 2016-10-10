3acda42253a0020ad36446b6346a5aa16b0d5a99
Lichao Wang (Dell laptop) <lichao.wang@uk.imshealth.com>
2016-10-10 17:27:34 +0100

    In progress: updating LinearRegression.py

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -8,62 +8,50 @@ from pyspark.sql.functions import rand
 from math import exp
 import numpy
 
-def addID(dataset, number, npar, name):
-    nPoses = dataset.count()
-    npFoldIDsPos = np.array(list(range(number)) * np.ceil(float(nPoses) / number))
-    # select the actual numbers of FoldIds matching the count of positive data points
-    npFoldIDs = npFoldIDsPos[:nPoses]
-    # Shuffle the foldIDs to give randomness
-    np.random.shuffle(npFoldIDs)
-    rddFoldIDs = sc.parallelize(npFoldIDs, npar).map(int)
-    dfDataWithIndex = dataset.rdd.zipWithIndex() \
-        .toDF() \
-        .withColumnRenamed("_1", "orgData")
-    dfNewKeyWithIndex = rddFoldIDs.zipWithIndex() \
-        .toDF() \
-        .withColumnRenamed("_1", "key")
-    dfJoined = dfDataWithIndex.join(dfNewKeyWithIndex, "_2") \
-        .select("orgData.matched_positive_id", 'key') \
-        .withColumnRenamed('key', name) \
-        .coalesce(npar)
-    return dfJoined
-
 def main():
-    nEvalFolds = 5
-    nValiFolds = 5
-    seed = 1
-    # hyper-params
+    # user to specify: hyper-params
     logLambdas = list(numpy.arange(-2,3,1))
     lambdas = map(exp, logLambdas)
     alphas = list(numpy.arange(0,1.01,0.1))
-
-    dataFileName = "s3://emr-rwes-pa-spark-dev-datastore/lichao.test/data/toy_data/data_linear_regression.csv"
-    spark = SparkSession\
-           .builder\
-           .appName("Linear Regression")\
-           .getOrCreate()
+    # user to specify: input data location
+    dataFileName = "s3://emr-rwes-pa-spark-dev-datastore/lichao.test/data/toy_data/data_LinearRegressionTemplate.csv"
+    # read data    
+    spark = SparkSession.builder.appName(__file__).getOrCreate()    
+    data = spark.read.option("header", "true").option("inferSchema", "true").csv(dataFileName)
+    # user to specify: original column names for stratification IDs in data
+    outerFoldCol = "OuterFoldID"
+    innerFoldCol = "InnerFoldID"
+    # user to specify: original column names for predictors and output in data
+    orgOutputCol = "y"
+    orgPredictorCols = data.columns[1:-2]
+    # user to specify: the collective column name for all predictors
+    collectivePredictorCol = "features"
+    # user to specify: the column name for prediction
+    predictionCol = "prediction"
     
-    data = spark\
-          .read\
-          .option("header", "true")\
-          .option("inferSchema", "true")\
-          .csv(dataFileName)
-          
-    assembler = VectorAssembler(inputCols=data.columns[1:], outputCol="features")
-    featureAssembledData = assembler.transform(data).select("y", "features")    
-    randCol = "rand_num"    
-    # evalRandColAppendedData = featureAssembledData.select("*", rand(seed).alias(randCol))
-    evalRandColAppendedData = featureAssembledData.withColumn(randCol, rand(seed))
+    # sanity check
+    if outerFoldCol not in data.columns:
+        raise ValueError("outerFoldCol " + outerFoldCol + " doesn't exist in the input data. ")
+    if innerFoldCol not in data.columns: 
+        raise ValueError("innerFoldCol " + innerFoldCol + " doesn't exist in the input data. ")
+    rowsUniqueOutFoldIDs = data.select(outerFoldCol).distinct().collect()
+    listUniqueOutFoldIDs = [x[outerFoldCol] for x in rowsUniqueOutFoldIDs]
+    if (set(listUniqueOutFoldIDs) != set(range(max(listUniqueOutFoldIDs)+1))):
+        raise ValueError("The outerFoldCol column " + outerFoldCol + " does not have zero-based consecutive integers as fold IDs.")
+     
     
-    lr = LinearRegression(maxIter=1e5, regParam=0.01, elasticNetParam=0.9, standardization=True, 
-                          featuresCol="features", labelCol="y")
-    evaluator = RegressionEvaluator(predictionCol="prediction", labelCol="y")
+    # convert to ml-compatible format
+    assembler = VectorAssembler(inputCols=orgPredictorCols, outputCol=collectivePredictorCol)
+    featureAssembledData = assembler.transform(data).select(orgOutputCol, collectivePredictorCol)    
+    
+    
+    # the model (pipeline)
+    lr = LinearRegression(maxIter=1e5, standardization=True, featuresCol=collectivePredictorCol, labelCol=orgOutputCol)
+    evaluator = RegressionEvaluator(predictionCol=predictionCol, labelCol=orgOutputCol)
     paramGrid = ParamGridBuilder()\
                .addGrid(lr.regParam, lambdas)\
                .addGrid(lr.elasticNetParam, alphas)\
                .build()    
-    
-    h = 1.0 / nEvalFolds
     
     predictionsAllData = None
     for iFold in range(nEvalFolds):
