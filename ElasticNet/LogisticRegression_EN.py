5e1e4dbcd35aeed379b0d086833e30fca3d114e6
Hui Jin(local) <hjin@cn.imshealth.com>
2016-10-18 14:56:42 +0800

    LogisticRegression_EN version 0.2 1. finish testing with Titanic data, running fine 2. add instructions for the template 3. need to cross-check with R/Python results

--- ElasticNet/LogisticRegression_EN.py
+++ ElasticNet/LogisticRegression_EN.py
@@ -2,16 +2,56 @@
 
 Instructions:
 
-0.
+0.  This script serves as a template for using logistic regression with the
+    elastic-net penalty and an outer plus an inner cross-validation for hyper-parameter selection.
+    Both the outer and the inner cross-validation loops uses predefined fold IDs included in the input data.
 
-This function could run LR Elastic-Net with Spark-submit in Spark ML module.
-elasticNetParam corresponds to alpha and regParam corresponds to lambda
-1. elasticNetParam is in range [0, 1]. 
-    For alpha = 0, the penalty is an L2 penalty. 
-    For alpha = 1, it is an L1 penalty.
-2. regParam is regularization parameter (>= 0).
+    Using the template, it is possible to obtain the following outputs:
+        0.1. The AUC values of all hyper-parameter sets in every outer cross-evaluation round;
+        0.2. The best hyper-parameter set in every outer cross-evaluation round;
+        0.3. Predictions(probability of label 1) and true label for the entire input data (across all evaluation rounds);
+        0.4. The overall AUC and AUPR values for the entire input data.
+
+1.  How to run the code: The template code depends on the module imspacv (currently in CrossValidator/imspacv.py).
+    As an example command for submitting the script, run
+
+    sudo spark-submit --deploy-mode client --master yarn --num-executors 5
+    --executor-cores 16 --executor-memory 19g --py-files /path/to/imspacv.py
+    /path/to/LogisticRegression_EN.py
+
+2.  How to update the template for different specifications:
+    2.1 In general, important fields to specify are listed at the beginning of the main function.
+        Please refer to the comments in the code. The following is the details:
+        2.1.1.  1D grids for lambda and alpha:
+                Please specify separately the grids for the hyper-parameters lambda and alpha.
+                Lambda is the overall weight of the regularizer (>0) and alpha controls the relative weights between the L1 and L2 norms (0<=alpha<=1)
+                Alpha=1 it is equivalent to a LASSO model, alpha=0 it is a ridge model.
+                Please specify each grid as a list.
+        2.1.2.  The input datafile name:
+                It needs to be a location on s3.
+                The data needs to include the following information:
+                    i. an output column,
+                    ii. one or more predictor columns
+                    iii. two columns of outer and inner stratification fold ID information.
+        2.1.3.  (Optional) You could specify the app name by relpacing the variable "__file__" with your preferred name.
+        2.1.4.  The column names of the outer and inner stratification fold IDs.
+                The program uses them to find stratification information stored in the input data.
+        2.1.5.  The column name of the output / dependent variable.
+        2.1.6.  The column names of the predictors (as a list).
+        2.1.7.  The desired collective column name for all predictors, e.g. "features".
+        2.1.8.  The desired name for the prediction column, e.g., "probability".
+        2.1.9.  The output location on both s3 and the master node, preferably different every time the program is run.
+                This could be achieved by e.g., using the current timestamp as the folder name.
+
+    2.2 Other fields to specify. The following is some possibilities but not an exhaustive list:
+        2.2.1.  If one needs to overwrite an existing csv file on s3, add the argument mode="overwrite" when calling DataFrame.write.csv();
+        2.2.2.  One could specify the file names for various outputs
+
+
+
+sudo spark-submit --deploy-mode client --master yarn --num-executors 5 --executor-cores 16 --executor-memory 19g --py-files /home/hjin/template_test/code/imspacv.py /home/hjin/template_test/code/LogisticRegression_EN.py
+
 '''
-
 
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, DoubleType, IntegerType, StringType
@@ -25,7 +65,6 @@ import time
 import datetime
 from imspacv import CrossValidatorWithStratificationID
 
-
 def main():
     # user to specify: hyper-params
     lambdas = [0.1, 1]
@@ -34,7 +73,9 @@ def main():
     dataFileName = "s3://emr-rwes-pa-spark-dev-datastore/Hui/template_test/data/data_LRRFTemplat_FoldID.csv"
     # read data
     spark = SparkSession.builder.appName(__file__).getOrCreate()
-    data = spark.read.option("header", "true").option("inferSchema", "true").csv(dataFileName)
+    data = spark.read.option("header", "true")\
+        .option("inferSchema", "true")\
+        .csv(dataFileName)
     # user to specify: original column names for stratification IDs in data
     outerFoldCol = "OuterFoldID"
     innerFoldCol = "InnerFoldID"
@@ -44,11 +85,11 @@ def main():
     # user to specify: the collective column name for all predictors
     collectivePredictorCol = "features"
     # user to specify: the column name for prediction
-    predictionCol = "rawPrediction"
+    predictionCol = "probability"
     # user to specify: the output location on s3
     start_time = time.time()
     st = datetime.datetime.fromtimestamp(start_time).strftime('%Y%m%d_%H%M%S')
-    resultDir_s3 = "s3://emr-rwes-pa-spark-dev-datastore/Hui/results/" + st + "/"
+    resultDir_s3 = "s3://emr-rwes-pa-spark-dev-datastore/Hui/template_test/results/" + st + "/"
     # user to specify the output location on master
     resultDir_master = "/home/hjin/template_test/results/" + st + "/"
 
@@ -73,7 +114,8 @@ def main():
     # the model (pipeline)
     lr = LogisticRegression(maxIter=1e5, featuresCol = collectivePredictorCol,
                             labelCol = orgOutputCol, standardization = True)
-    evaluator = BinaryClassificationEvaluator(predictionCol=predictionCol, labelCol=orgOutputCol)
+    evaluator = BinaryClassificationEvaluator(rawPredictionCol=predictionCol,
+                                              labelCol=orgOutputCol)
     paramGrid = ParamGridBuilder()\
                .addGrid(lr.regParam, lambdas)\
                .addGrid(lr.elasticNetParam, alphas)\
@@ -110,21 +152,25 @@ def main():
         # save the metrics for all hyper-parameter sets in cv
         cvMetrics = cvModel.avgMetrics
         cvMetricsFileName = resultDir_s3 + "cvMetricsFold" + str(iFold)
-        cvMetrics.coalesce(4).write.csv(cvMetricsFileName, header="true")
+        cvMetrics.coalesce(1).write.csv(cvMetricsFileName, header="true")
         # save the hyper-parameters of the best model
         bestParams = validator.getBestModelParams()
-        fileBestParams = open(resultDir_master + "bestParamsFold" + str(iFold) + ".txt", "w")
-        fileBestParams.writelines(str(bestParams))
-        fileBestParams.close()
+        with open(resultDir_master + "bestParamsFold" + str(iFold) + ".txt",
+                  "w") as fileBestParams:
+            fileBestParams.write(str(bestParams))
         # save coefficients of the best model
-        fileCoef = open(resultDir_master + "coefsFold" + str(iFold) + ".txt", "w")
-        fileCoef.writelines("Intercept: {}\n".format(str(cvModel.bestModel.intercept)))
-        fileCoef.writelines("Coefficients: {}\n".format(str(cvModel.bestModel.coefficients)))
-        fileCoef.close()
+        with open(resultDir_master + "coefsFold" + str(iFold) + ".txt",
+                  "w") as fileCoef:
+            fileCoef.write("Intercept: {}".format(str(cvModel.bestModel.intercept)))
+            fileCoef.write("\n")
+            for id in range(len(orgPredictorCols)):
+                fileCoef.write("%s : %f" %(orgPredictorCols[id] ,cvModel.bestModel.coefficients[id]))
+                fileCoef.write("\n")
 
     # save all predictions
     predictionsFileName = resultDir_s3 + "predictionsAllData"
-    predictionsAllData.select(orgOutputCol, predictionCol).write.csv(predictionsFileName, header="true")
+    predictionsAllData.select(orgOutputCol, predictionCol)\
+        .write.csv(predictionsFileName, header="true")
     # save AUC and AUPR
     auc = evaluator.evaluate(predictionsAllData,
                              {evaluator.metricName:"areaUnderROC"})
